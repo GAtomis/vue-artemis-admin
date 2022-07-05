46740c74a404452d5547b65edd1006af498b09a7
Gavin <zhounan@agora.io>
2022-07-05 15:17:41 +0800

    update@mock

--- mock/Dao/User.ts
+++ mock/Dao/User.ts
@@ -4,7 +4,7 @@ import { Interface } from 'readline'
  * @Description: User Tao声明
  * @Author: Gavin
  * @Date: 2021-08-04 16:55:59
- * @LastEditTime: 2021-11-19 15:27:08
+ * @LastEditTime: 2022-07-05 15:10:18
  * @LastEditors: Gavin
  */
 export interface UserInfo {
