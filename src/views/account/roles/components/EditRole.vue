1593e13777a294c1b6dfb5dc39537b5d74a7b959
Gavin <850680822@qq.com>
2022-11-15 18:27:51 +0800

    update@theme

--- src/views/account/roles/components/EditRole.vue
+++ src/views/account/roles/components/EditRole.vue
@@ -2,7 +2,7 @@
  * @Author: Gavin 850680822@qq.com
  * @Date: 2022-11-10 23:23:43
  * @LastEditors: Gavin 850680822@qq.com
- * @LastEditTime: 2022-11-11 16:37:29
+ * @LastEditTime: 2022-11-15 17:58:28
  * @FilePath: \vue-artemis-admin\src\views\account\roles\components\UserInfo.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -70,7 +70,7 @@ const labelCol = { span: 4 },
     onFinish = async () => {
         //edit&new
 
-        await updateItem(form)
+        await updateItem(form.value)
         message.success('submit success ')
         getItem({ id: currentItem?.value.id }).then((res) => {
             form.value = res
